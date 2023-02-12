9055285f6e222ca620065ee34f5afe98b3e8139e
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-02-12 14:17:42 +0800

    change minSdk
    ERROR:/home/runner/.gradle/caches/modules-2/files-2.1/org.apache.logging.log4j/log4j-api/2.19.0/ea1b37f38c327596b216542bc636cfdc0b8036fa/log4j-api-2.19.0.jar: D8: com.android.tools.r8.internal.k2: MethodHandle.invoke and MethodHandle.invokeExact are only supported starting with Android O (--min-api 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "top.mrxiaom.mirai.aoki"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 32
         versionCode = 2
         versionName = "1.1.1"
