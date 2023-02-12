25958f642be9d7b495014909eb31f05dee0740e1
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-02-12 14:07:54 +0800

    update 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "top.mrxiaom.mirai.aoki"
         minSdk = 24
         targetSdk = 32
-        versionCode = 1
-        versionName = "1.1.0"
+        versionCode = 2
+        versionName = "1.1.1"
     }
 
     buildTypes {
