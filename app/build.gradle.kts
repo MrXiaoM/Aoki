190c24d132bb7d927ac2f092cf729bcdab7c62a3
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-03-08 12:58:07 +0800

    Update 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "top.mrxiaom.mirai.aoki"
         minSdk = 26
         targetSdk = 32
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.1.2"
 
         buildConfigField("String", "miraiVersion", "\"$miraiVersion\"")
     }
