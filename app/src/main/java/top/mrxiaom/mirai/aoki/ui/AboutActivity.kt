a70ebe2e63bf25c9be1ca77edf5912f7cc8b5290
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-03-23 18:36:53 +0800

    add back button to AboutActivity

--- app/src/main/java/top/mrxiaom/mirai/aoki/ui/AboutActivity.kt
+++ app/src/main/java/top/mrxiaom/mirai/aoki/ui/AboutActivity.kt
@@ -3,6 +3,7 @@ package top.mrxiaom.mirai.aoki.ui
 import android.os.Bundle
 import android.text.Html
 import android.text.method.LinkMovementMethod
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import top.mrxiaom.mirai.aoki.R
 import top.mrxiaom.mirai.aoki.databinding.ActivityAboutBinding
@@ -21,5 +22,15 @@ class AboutActivity : AppCompatActivity() {
             text = Html.fromHtml(text(R.string.about_body), 0)
             movementMethod = LinkMovementMethod.getInstance()
         }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish();
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
