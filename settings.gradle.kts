0dbbbf9a699454fad02deaa19cefb8e76e6da118
ヒューマンワーキン <coolxiaom95@gmail.com>
2023-03-24 12:50:50 +0800

    add huawei cloud mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven("https://repo.huaweicloud.com/repository/maven")
         google()
         mavenCentral()
         maven("https://repo.mirai.mamoe.net/snapshots")
