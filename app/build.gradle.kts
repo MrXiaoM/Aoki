c0000f04c3cae8dc8e4e488efca30bf60e08e7cd
MrXiaoM <coolxiaom95@gmail.com>
2023-06-13 18:59:02 +0800

    add module `tlv544_enc`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("net.mamoe:mirai-core-utils:$miraiVersion")
 
     implementation("com.youbenzi:MDTool:1.2.4")
+    implementation(project(":tlv544_enc"))
 
     implementation("org.bouncycastle:bcprov-jdk15on:1.67")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "Aoki"
 include(":app")
+include(":tlv544_enc")
